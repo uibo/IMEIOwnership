8992a0732e242bdb8b7f09611781caa73a839207
uibo <boui000210@gmail.com>
2025-06-12 11:55:53 +0900

    signature 메시지 생성방식 metamask와 호환되게 변경

--- Contract/src/IMEIOwnership.sol
+++ Contract/src/IMEIOwnership.sol
@@ -62,7 +62,7 @@ contract IMEIOwnership {
     }
 
     function _recoverSigner(bytes memory message, bytes memory signature) internal pure returns (address) {
-        bytes32 ethSignMsgHash = MessageHashUtils.toEthSignedMessageHash(keccak256(message));
+        bytes32 ethSignMsgHash = MessageHashUtils.toEthSignedMessageHash(message);
         return ECDSA.recover(ethSignMsgHash, signature);
     }
 
@@ -74,7 +74,10 @@ contract IMEIOwnership {
 
     function registerIMEI(bytes32 imeiHash, address to, uint nonce, bytes calldata signature) external onlyContractOwner notRegistered(imeiHash) {
         _validNonce(to, nonce);
-        bytes memory message = abi.encodePacked("registerIMEI ", imeiHash, " to ", to, " (nonce: ", Strings.toString(nonce), ")");
+        string memory str_imeiHash = Strings.toHexString(uint256(imeiHash));
+        string memory str_to = Strings.toHexString(to);
+        string memory str_nonce = Strings.toString(nonce);
+        bytes memory message = abi.encodePacked("registerIMEI ", str_imeiHash, " to ", str_to, " nonce ", str_nonce);
         address signer = _recoverSigner(message, signature);
         require(signer == to, "Signer discord");
         imeiHashToOwner[imeiHash] = signer;
@@ -82,7 +85,11 @@ contract IMEIOwnership {
 
     function transferIMEI(bytes32 imeiHash, address from, address to, uint nonce, bytes calldata signature) external {
         _validNonce(from, nonce);
-        bytes memory message = abi.encodePacked("transferIMEI ", imeiHash, " from ", from, " to ", to, " (nonce: ", Strings.toString(nonce), ")");
+        string memory str_imeiHash = Strings.toHexString(uint256(imeiHash));
+        string memory str_from = Strings.toHexString(from);
+        string memory str_to = Strings.toHexString(to);
+        string memory str_nonce = Strings.toString(nonce);
+        bytes memory message = abi.encodePacked("transferIMEI ", str_imeiHash, " from ", str_from, " to ", str_to, " nonce ", str_nonce);
         address signer = _recoverSigner(message, signature);
         require(signer == from, "Signer discord");
         _onlyIMEIOwner(imeiHash, signer);
@@ -94,19 +101,25 @@ contract IMEIOwnership {
         _validNonce(tradeInfo.buyer, buyerInfo.nonce);
         
         require(imeiHashToEscrow[tradeInfo.imeiHash].isPending == false, "This IMEI is trading!");
+        string memory str_imeiHash = Strings.toHexString(uint256(tradeInfo.imeiHash));
+        string memory str_from = Strings.toHexString(tradeInfo.seller);
+        string memory str_to = Strings.toHexString(tradeInfo.buyer);
+        string memory str_price = Strings.toString(tradeInfo.price);
+        string memory str_sellerInfo_nonce = Strings.toString(sellerInfo.nonce);
         bytes memory sellerMessage = abi.encodePacked(
-            "tradeIMEI ", tradeInfo.imeiHash, 
-            " from ", tradeInfo.seller, 
-            " to ", tradeInfo.buyer, 
-            " price: ", Strings.toString(tradeInfo.price), 
-            " (nonce: ", Strings.toString(sellerInfo.nonce), ")"
+            "tradeIMEI ", str_imeiHash,
+            " from ", str_from ,
+            " to ", str_to,
+            " price ", str_price,
+            " nonce ", str_sellerInfo_nonce
         );
+        string memory str_buyerInfo_nonce = Strings.toString(buyerInfo.nonce);
         bytes memory buyerMessage = abi.encodePacked(
-            "tradeIMEI ", tradeInfo.imeiHash, 
-            " from ", tradeInfo.seller, 
-            " to ", tradeInfo.buyer, 
-            " price: ", Strings.toString(tradeInfo.price), 
-            " (nonce: ", Strings.toString(buyerInfo.nonce), ")"
+            "tradeIMEI ", str_imeiHash,
+            " from ", str_from, 
+            " to ", str_to,
+            " price ", str_price, 
+            " nonce ", str_buyerInfo_nonce
         );
         address sellerSigner = _recoverSigner(sellerMessage, sellerInfo.signature);
         address buyerSigner = _recoverSigner(buyerMessage, buyerInfo.signature);
@@ -121,7 +134,9 @@ contract IMEIOwnership {
     }
 
     function confirmTrade(bytes32 imeiHash, uint nonce, bytes calldata signature) external {
-        bytes memory message = abi.encodePacked("confirmTrade ", imeiHash, " (nonce: ", Strings.toString(nonce), ")");
+        string memory str_imeiHash = Strings.toHexString(uint256(imeiHash));
+        string memory str_nonce = Strings.toString(nonce);
+        bytes memory message = abi.encodePacked("confirmTrade ", str_imeiHash, " nonce ", str_nonce);
         address buyerSigner = _recoverSigner(message, signature);
         require(imeiHashToEscrow[imeiHash].buyer == buyerSigner, "Not buyer");
         require(imeiHashToEscrow[imeiHash].isPending == true, "Trade already completed");
